ab3ddcaa75b46506601dbca137bc3f048a17521b
leegoonz <leegoon73@gmail.com>
2020-02-07 03:58:44 +0800

    Switch  Material class Init

---
+++ Assets/Scripts/ArtistMaterialSetLOD.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+[System.Serializable]
+[ExecuteInEditMode]
+public enum EditableMaterialsList
+{
+    Lod0,
+    Lod1,
+    Lod2
+};
+ 
+[ExecuteInEditMode]
+public class ArtistMaterialSetLOD : MonoBehaviour
+{
+    public bool editMode = true;
+    public Material mat1;
+    public Material mat2;
+    public Material mat3;
+    public EditableMaterialsList editableMaterialsList = EditableMaterialsList.Lod0;
+
+    private Renderer _rend;
+    private Renderer _renderer;
+    private void Start()
+    {
+        _renderer = GetComponent<Renderer>();
+    }
+#if UNITY_EDITOR
+    private void UpdateMatLod()
+    {
+        _rend = _renderer;
+        if (UnityEditor.EditorApplication.isPlaying)
+        {
+            editMode = false;
+        }
+        else editMode = true;
+ 
+        if (!editMode)
+        {
+            return;
+        }
+        else
+        {
+            switch (editableMaterialsList)
+            {
+                case EditableMaterialsList.Lod0:
+                    _rend.material = mat1;
+                    break;
+                case EditableMaterialsList.Lod1:
+                    _rend.material = mat2;
+                    break;
+                case EditableMaterialsList.Lod2:
+                    _rend.material = mat3;
+                    break;
+                default:
+                    break;
+            }
+        }
+      
+    }
+
+
+    private void Update()
+    {
+        UpdateMatLod();
+    }
+#endif
+ 
+}
---
+++ Assets/Scripts/SwitchMaterialSwapLod.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine.Serialization;
+
+[System.Serializable]
+[ExecuteInEditMode]
+public class SwitchMaterialSwapLod : MonoBehaviour
+{
+
+    public int[] matLodDistance;
+    public Material[] lodMat;
+    Renderer _rend;
+    [NonSerialized] public float UpdataTime = 0.1f;
+    private SkinnedMeshRenderer _skinnedMeshRenderer;
+    private Camera _camera;
+
+
+    void Start()
+    {
+        _camera = Camera.main;
+        _skinnedMeshRenderer = this.GetComponent<SkinnedMeshRenderer>();
+        _rend = GetComponent<Renderer>();
+        StartCoroutine(nameof(UpdateMatLod));
+    }
+
+
+
+    IEnumerator UpdateMatLod()
+    {
+        var period = UpdataTime;
+        if (_camera != null)
+        {
+            var cam = _camera.transform;
+            while (true)
+            {
+                var distance = (int)(transform.position - cam.position).sqrMagnitude;
+                
+                // ReSharper disable once Unity.PerformanceCriticalCodeInvocation
+                Debug.Log("type cast distance : " + distance);
+                
+                // make LOD checking non deterministic. avoids stampeding LOD calls?
+                yield return new WaitForSeconds(period);
+            }
+        }
+    }
+}

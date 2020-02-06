3a25683bb7120bfebdde3184ca7dda6b4010e6f7
leegoonz <leegoon73@gmail.com>
2020-02-07 04:24:39 +0800

    Material Change mathod fixed by distance

--- Assets/Scripts/SwitchMaterialSwapLod.cs
+++ Assets/Scripts/SwitchMaterialSwapLod.cs
@@ -36,10 +36,25 @@ public class SwitchMaterialSwapLod : MonoBehaviour
             var cam = _camera.transform;
             while (true)
             {
-                var distance = (int)(transform.position - cam.position).sqrMagnitude;
-                
+                var distance = (transform.position - cam.position).sqrMagnitude;
+                int intDistance = Mathf.RoundToInt(distance);
+                if (intDistance > matLodDistance[2])
+                {
+                    Debug.Log("*************************current mat is LOD2");
+                    _rend.material = lodMat[2];
+                }
+                else if (intDistance > matLodDistance[1])
+                {
+                    Debug.Log("*************current mat is LOD1");
+                    _rend.material = lodMat[1];
+                }
+                else if(intDistance > matLodDistance[0])
+                {
+                    Debug.Log("*****current mat is LOD0");
+                    _rend.material = lodMat[0];
+                }
                 // ReSharper disable once Unity.PerformanceCriticalCodeInvocation
-                Debug.Log("type cast distance : " + distance);
+                Debug.Log("type cast distance : " + intDistance);
                 
                 // make LOD checking non deterministic. avoids stampeding LOD calls?
                 yield return new WaitForSeconds(period);
